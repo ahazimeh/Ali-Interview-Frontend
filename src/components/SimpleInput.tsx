c7e0ed8c9fcec3d669e211f61ba7e0d14b3bbdae
ali-pixel38 <ali@pixel38.com>
2022-09-10 23:06:30 +0300

    first commit

---
+++ src/components/SimpleInput.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import useInput from '../hooks/use-input';
+interface Props {
+    children?: React.ReactNode
+}
+const SimpleInput: React.FC<Props> = (props) => {
+    const {
+        value: enteredName,
+        isValid: enteredNameIsValid,
+        hasError: nameInputHasError,
+        valueChangeHandler: nameChangeHandler,
+        inputBlurHandler: nameBlurHandler,
+        reset: resetNameInput
+    } = useInput((value) => value.trim() !== '');
+    // const [enteredName, setEnteredName] = useState('');
+    // const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+    // const enteredNameIsValid = enteredName.trim() !== ''
+    // const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+
+    let formIsValid = false;
+    if (enteredNameIsValid) { //add all validities 
+        formIsValid = true;
+    }
+
+    const formSubmissionHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+
+        if (!enteredNameIsValid) {
+            return;
+        }
+        console.log(enteredName)
+        resetNameInput()
+    }
+
+
+    const nameInputClasses = nameInputHasError ? 'form-control invalid' : 'form-control'
+
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            <div className={nameInputClasses}>
+                <label htmlFor="name">Your Name</label>
+                <input type="text" id="name"
+                    onChange={nameChangeHandler}
+                    onBlur={nameBlurHandler}
+                    value={enteredName} />
+                {nameInputHasError && <p className='error-text'>Name must not be empty</p>}
+            </div>
+            <div className='form-actions'>
+                <button disabled={!formIsValid}>Submit</button>
+            </div>
+        </form>
+    )
+}
+
+export default SimpleInput;
